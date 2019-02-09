bb1237a5529364d461ea498a40666d6719e03296
OSCAR RIOS <oscr.rss@gmail.com>
2019-02-09 22:33:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,12 @@ def countdown(number)
 end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  sleep 5
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+  return "HAPPY NEW YEAR!"
+end
